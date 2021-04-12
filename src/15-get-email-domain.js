130429a1338cae9a06a3adc33468cd1c1b1acfca
ileudo <Rios111@yandex.ru>
2021-04-13 00:29:27 +0300

    feat: implement task 15 solution

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const regex = /(?!.*@.*).*/gs;
+  const x = email.match(regex)[0];
+  return x;
 }
-
 module.exports = getEmailDomain;
