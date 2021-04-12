0831dfb844440052a3aaed9a233990b5ac915445
ileudo <Rios111@yandex.ru>
2021-04-12 13:45:53 +0300

    feat: implememnt task 11 solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') return '';
+  const arr = str.split('');
+  let result = '';
+  let counter = 1;
+  let prev;
+  arr.reduce((acc, current) => {
+    prev = acc;
+    if (current === prev) {
+      counter += 1;
+    } else {
+      if (counter > 1) { result += `${counter}${prev}`; } else { result += `${prev}`; }
+      counter = 1;
+    }
+    prev = current;
+    return prev;
+  });
+  if (counter > 1) { result += `${counter}${prev}`; } else { result += `${prev}`; }
+  return result;
 }
-
 module.exports = encodeLine;
